7b983cc713a84c7160c99c2672a5eb860b6f3eff
howardbdev <howarddevennish@gmail.com>
2020-04-14 13:31:22 -0400

    add routes for users login logout and get current user

--- backend-with-auth/config/routes.rb
+++ backend-with-auth/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      post "/login", to: "sessions#new"
+      post "/logout", to: "sessions#destroy"
+      get "/get_current_user", to: "sessions#get_current_user"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
