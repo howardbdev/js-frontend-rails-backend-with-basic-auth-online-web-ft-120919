01c4b8a7c02d56e1387e5ed92ab6f1b6d822bde0
howardbdev <howarddevennish@gmail.com>
2020-04-14 22:01:55 -0400

    move load main to DOM class

--- frontend/services/dom.js
+++ frontend/services/dom.js
@@ -3,4 +3,22 @@ class DOM {
     const navDiv = document.getElementById("nav")
     navDiv.innerHTML = Navbar.render()
   }
+
+  static loadLoginForm() {
+    const main = document.getElementById("main")
+    main.innerHTML = Auth.renderLoginForm
+  }
+
+  static loadMainContainer() {
+    this.resetNav()
+    if (Auth.isSignedIn) {
+      this.loadUserDashboard()
+    } else {
+      this.loadLoginForm()
+    }
+  }
+
+  static loadUserDashboard() {
+    
+  }
 }
