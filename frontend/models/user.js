1f8223edfd0e8619efa55dfb2cce413ceca0ec10
howardbdev <howarddevennish@gmail.com>
2020-04-14 22:23:46 -0400

    add signup form

--- frontend/models/user.js
+++ frontend/models/user.js
@@ -3,4 +3,14 @@ class User {
     this.id = attributes.id
     this.email = attributes.email
   }
+
+  static get renderSignupForm() {
+    return `
+      <form class="auth-form" id="signup-form" action="#" method="post">
+        <input id="signup-form-email-input" type="text" name="email" value="" placeholder="email">
+        <input id="signup-form-password-input" type="password" name="password" value="" placeholder="password">
+        <input class="auth-form" id="signup-form-submit" type="submit" value="Sign Up">
+      </form>
+    `
+  }
 }
